1dbf8930a95de67d8d1d16dede4614c71f4c263d
Peter.Grecian <peter.grecian@nice.com>
2026-01-05 11:16:37 +0000

    Export DHT11Component from producers package

--- components/producers/__init__.py
+++ components/producers/__init__.py
@@ -3,5 +3,6 @@ Producer components (sensors, inputs)
 """
 
 from .dht22 import DHT22Component
+from .dht11 import DHT11Component
 
-__all__ = ['DHT22Component']
+__all__ = ['DHT22Component', 'DHT11Component']
